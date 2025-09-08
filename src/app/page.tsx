43023731b2f36d7ebd196c6ef7fdc450d7ef0ee4
Dhananjay Sahoo <kanha94377@gmail.com>
2025-09-08 16:06:36 +0530

    loading screen added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-white">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-purple-600 border-solid"></div>
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-600 border-solid"></div>
     </div>
   );
 }
